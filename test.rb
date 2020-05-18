3cbab37eb145147e60d787ea7e51559c101d6b53
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-18 20:35:26 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,8 +1,9 @@
 count = 11
 
+if count != 0 
 while count > 0 do 
   puts "#{count}"
   count = count - 1
-  if "#{count = 0}" then 
-    puts "Happy New Year!"
+else 
+  puts "Happy New Year!"
 end
